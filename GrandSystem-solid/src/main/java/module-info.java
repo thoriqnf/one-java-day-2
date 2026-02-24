7cf44efb61c39ae2884b668a51c3e0d5f4a47ca5
Thoriq Nur Faizal <thoriqnfaizal@gmail.com>
2026-02-24 15:33:29 +0700

    chores: remove unused file

--- GrandSystem-solid/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.Blubca.finance {
-    requires java.sql;
-    exports com.Blubca.finance;
-    exports com.Blubca.finance.domain;
-    exports com.Blubca.finance.repository;
-    exports com.Blubca.finance.service;
-}
